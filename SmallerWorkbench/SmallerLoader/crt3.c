24696dcc190be6a713ba9e331f378c2ce0ea1450
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-02-03 13:28:49 +0100

    Update crt3.c

--- SmallerWorkbench/SmallerLoader/crt3.c
+++ SmallerWorkbench/SmallerLoader/crt3.c
@@ -1,21 +1,22 @@
-#include <stdlib.h>
-
-typedef struct
-{
-	int newmode;
-} _startupinfo;
-
-void __set_app_type(int);
-void _controlfp(unsigned a, unsigned b);
-void __getmainargs(int *pargc, char ***pargv, char ***penv, int globb, _startupinfo* s);
-int main(int c, char **a, char **e);
-
-int _start(void) {
-	int c;
-	char **a,**e;
-	_startupinfo s = {0};
-	_controlfp(0x10000, 0x30000);
-	__set_app_type(1);
-	__getmainargs(&c, &a, &e, 0, &s);
-	exit(main(c,a,e));
-}
+.text
+  .global _start
+_start:
+  subq $40, %rsp
+  movl $196608, %esi
+  movl $65536, %edi
+  movl $0, 12(%rsp)
+  call _controlfp
+  movl $1, %edi
+  call __set_app_type
+  leaq 12(%rsp), %r8
+  leaq 24(%rsp), %rdx
+  leaq 16(%rsp), %rsi
+  leaq 8(%rsp), %rdi
+  xorl %ecx, %ecx
+  call __getmainargs
+  movl 8(%rsp), %edi
+  movq 24(%rsp), %rdx
+  movq 16(%rsp), %rsi
+  call main
+  movl %eax, %edi
+  call exit
